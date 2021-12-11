7de9e3a1f035fd49528c85c854fcbf99f72d8804
Ivana Cliffords <72675474+IvanaCliffords@users.noreply.github.com>
2021-12-10 21:57:57 -0600

    Added classNames and divs for styling

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -10,15 +10,17 @@ function Welcome() {
 
   return (
     <div className="welcome-div">
-      <h1 className="welcome-h1">{credentials && `Welcome ${credentials.name}! Your balance is $${credentials.balance}`}
-      </h1>
-      <div className="links-div">
-        {!credentials && <Link to="/register" className="welcome-link">Register </Link>}
-        
-        {!credentials && <Link to="/login" className="welcome-link">Login</Link>}
+      <h1 className="welcome-h1">{credentials && `Welcome ${credentials.name}!`}</h1>
+      <h2 className="welcome-h2">{credentials && `Your balance is $${credentials.balance}`}</h2>
+      
+      {!credentials && <h4 className="welcome-h4">Please start by creating an account or logging in.</h4>}
+      {credentials && <h4 className="welcome-h4">Choose to either deposit or withdraw some money</h4>}
 
-        {credentials && <Link to="/deposit" className="welcome-link">Deposit</Link>}
-        {credentials && <Link to="/withdraw" className="welcome-link">Withdraw</Link>}
+      <div className="links-div">
+        {!credentials && <Link to="/register" className="welcome-link"><div className="link">Create Account</div></Link>}
+        {!credentials && <Link to="/login" className="welcome-link"><div className="link">Login</div></Link>}
+        {credentials && <Link to="/deposit" className="welcome-link"><div className="link">Deposit</div></Link>}
+        {credentials && <Link to="/withdraw" className="welcome-link"><div className="link">Withdraw</div></Link>}
       </div>
     </div>
   );
